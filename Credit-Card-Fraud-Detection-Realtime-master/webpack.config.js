0c1232519093e68b31fd76b3449496568c942b7f
panugantiguptha <158723450+panugantiguptha@users.noreply.github.com>
2026-08-18 11:12:30 +0530

    Create webpack.config.js
    Configures how the project files are built and bundled.

---
+++ Credit-Card-Fraud-Detection-Realtime-master/webpack.config.js
@@ -0,0 +1,42 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname + '/build',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    port: 3000,
+    host: '127.0.0.1'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './index.html',
+        to: './'
+      },
+      {
+        from: './style/*',
+        to: './'
+      },
+    ])
+  ]
+};
